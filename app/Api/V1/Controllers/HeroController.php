c308317b2cc86c3202bb79c9deca39925c1c59c9
Michal Konik <---->
2023-01-27 02:34:52 +0100

    project start

---
+++ app/Api/V1/Controllers/HeroController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
+class HeroController extends Controller
+{
+    public function getHeroes()
+    {
+
+    }
+}
